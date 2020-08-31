f727d2a34527e8773a24457c01adf99afe99ff70
神农西瓜 <63465872@qq.com>
2020-08-31 10:27:27 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
 // 导入全局样式
 import './assets/css/global.css'
 // 导入axios
@@ -9,6 +11,11 @@ import axios from 'axios'
 
 // 设置axios请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 设置拦截器
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 // 将axios挂载到vue全局对象上
 Vue.prototype.$http = axios
 
